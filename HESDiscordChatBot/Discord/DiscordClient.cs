1bf900dfa975bdd7bae666de3798daf9e434828a
Generalwrex <bjdroot@gmail.com>
2017-11-02 17:14:55 -0700

    Work in progress commit (checking webhook)

---
+++ HESDiscordChatBot/Discord/DiscordClient.cs
@@ -0,0 +1,21 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HESDiscordChatBot.Discord
+{
+    public class DiscordClient
+    {
+        private static DiscordSocketClient _client;
+
+        public DiscordClient()
+        {
+
+        }
+
+
+    }
+}
